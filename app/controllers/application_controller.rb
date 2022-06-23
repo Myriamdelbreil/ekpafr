d6e6c76fa912b382c07ff77c9cf4369e41f9a502
Myriam Delbreil <myriam.delbreil@live.fr>
2022-06-23 15:06:03 +0200

    pb cloudinary non résolu

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:site_connu, :diplome])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:site_connu, :diplome, :photo])
   end
 end
