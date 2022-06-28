f60506e4f2a551b748ea29852b9ff45bf4e65daa
Myriam Delbreil <myriam.delbreil@live.fr>
2022-06-28 12:51:09 +0200

    setup test local du contactform + en production, à retester

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'ekpafrance.heroku.com',
+    :domain         => 'ekpafrance.herokuapp.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
