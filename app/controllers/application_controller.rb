0d11a5563569de3642577fffbfbae0cf46b9d36c
Myriam Delbreil <myriam.delbreil@live.fr>
2022-06-28 12:22:45 +0200

    ?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:diplome, :site_connu, :email_confirmation, :nom, :prenom, :addresse, :telephone, :newsletter, :photo])
-    # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:diplome, :site_connu, :email_confirmation, :nom, :prenom, :addresse, :telephone, :newsletter, :photo])
   end
 end
