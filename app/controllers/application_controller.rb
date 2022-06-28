5bdaf54cce43c1e90209cd71d1bf3ac802862ada
Myriam Delbreil <myriam.delbreil@live.fr>
2022-06-28 12:12:04 +0200

    form sign_in fonctionne + avatar ok

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:diplome, :site_connu])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:diplome, :site_connu, :email_confirmation, :nom, :prenom, :addresse, :telephone, :newsletter, :photo])
     # devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
 end
