9be46cce3173219aa0bf7ab7da109d98b4e0f717
Myriam Delbreil <94977195+Myriamdelbreil@users.noreply.github.com>
2022-06-28 12:22:33 +0200

    Revert "Contactform2"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   ActionMailer::Base.smtp_settings = {
-    :port           => 587,
-    :address        => smtp.mailgun.org,
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
     :domain         => 'ekpafrance.heroku.com',
