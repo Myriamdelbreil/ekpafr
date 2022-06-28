de643f9c43d5185430dc86e1f2ac918ddb865a9a
Myriam Delbreil <myriam.delbreil@live.fr>
2022-06-28 15:19:38 +0200

    setup repris

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,11 +92,11 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   ActionMailer::Base.smtp_settings = {
-    :port           => 587,
+    :port           => ENV['MAILGUN_SMTP_PORT'],
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'ekpafrance.herokuapp.com',
+    :domain         => 'yourapp.heroku.com',
     :authentication => :plain,
   }
   ActionMailer::Base.delivery_method = :smtp
