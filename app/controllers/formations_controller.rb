bb2abcdeda6474a540b7003c4522cda769a6d01d
Myriam Delbreil <myriam.delbreil@live.fr>
2022-06-29 21:48:27 +0200

    début de l'index avec les requetes sql

--- app/controllers/formations_controller.rb
+++ app/controllers/formations_controller.rb
@@ -7,7 +7,7 @@ class FormationsController < ApplicationController
       @pagy, @formations = pagy(Formation.joins(:theme).where('sujet ILIKE ?', params[:query]), items: 9)
       @formations_count = Formation.joins(:theme).where('sujet ILIKE ?', params[:query]).count
       @search = params[:query]
-    elsif params[:sujet].nil? && params[:prix].nil? && params[:lieu].nil?
+    elsif params[:sujet].nil? && (params[:prix].nil? || params[:prix].to_i.zero?) && params[:lieu].nil?
       @themes = Theme.all
       @pagy, @formations = pagy(Formation.includes(:theme).all, items: 9)
     elsif !params[:sujet].nil? && !params[:sujet][:id].to_i.zero?
@@ -15,7 +15,10 @@ class FormationsController < ApplicationController
       @pagy, @formations = pagy(Theme.find(@param).formations, items: 9)
       @formations_count = Theme.find(@param).formations.count
       @search = Theme.find(@param).sujet
+    elsif (params[:sujet].nil? || params[:sujet][:id]=="") && (!params[:prix].nil? || !params[:prix].to_i.zero?)
+      @pagy, @formatons = pagy(Formation.where('prix <= ?', params[:prix]), items: 9)
     end
+
     # if params[:query].present?
     #   @pagy, @formations = pagy(Formation.joins(:theme).where('sujet ILIKE ?', params[:query]), items: 9)
     #   @formations_count = Formation.joins(:theme).where('sujet ILIKE ?', params[:query]).count
