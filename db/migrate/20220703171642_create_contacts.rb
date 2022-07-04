252e5c92bb11c68244c1dc189c855f0e7af9967d
Myriam Delbreil <myriam.delbreil@live.fr>
2022-07-04 18:00:21 +0200

    pending migrations

--- db/migrate/20220703171642_create_contacts.rb
+++ db/migrate/20220703171642_create_contacts.rb
@@ -1,11 +1,11 @@
-class CreateContacts < ActiveRecord::Migration[7.0]
-  def change
-    create_table :contacts do |t|
-      t.string :email
-      t.string :name
-      t.text :content
+# class CreateContacts < ActiveRecord::Migration[7.0]
+#   def change
+#     create_table :contacts do |t|
+#       t.string :email
+#       t.string :name
+#       t.text :content
 
-      t.timestamps
-    end
-  end
-end
+#       t.timestamps
+#     end
+#   end
+# end
