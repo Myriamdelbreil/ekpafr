22879c94e088b44599f71f41c05c0c667a3cc1e2
Myriam Delbreil <myriam.delbreil@live.fr>
2022-07-13 16:05:58 +0200

    à tester en prod

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
     from = "myriam.delbreil@live.fr"
     to = 'myriam.delbreil@live.fr'
     subject = ' '
-    content = Content.new(type: 'text/plain', value: "De : #{@contact.email} - #{@contact.name} - #{@contact.content}")
+    content = "De : #{@contact.email} - #{@contact.name} - #{@contact.content}"
     mail = Mail.create(from, subject, to, content)
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
