975697640a5f61383eabef73cd06104fa6dff4a5
Myriam Delbreil <myriam.delbreil@live.fr>
2022-07-13 16:08:09 +0200

    create remis en .new

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,7 +10,7 @@ class ContactsController < ApplicationController
     to = 'myriam.delbreil@live.fr'
     subject = ' '
     content = "De : #{@contact.email} - #{@contact.name} - #{@contact.content}"
-    mail = Mail.create(from, subject, to, content)
+    mail = Mail.new(from, subject, to, content)
     sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
     response = sg.client.mail._('send').post(request_body: mail.to_json)
     puts response.status_code
