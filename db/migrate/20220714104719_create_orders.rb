155e3e35783e3fba1db6824420a182bb9186e23c
Myriam Delbreil <myriam.delbreil@live.fr>
2022-07-15 16:14:52 +0200

    stripe fonctionne en local, reste à pouvoir update le status de l'order en DB & créer le reçu et l'inscription dès que le paiement est validé

---
+++ db/migrate/20220714104719_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.string :state
+      t.integer :amount
+      t.string :checkout_session_id
+      t.references :user, null: false, foreign_key: true
+      t.references :formation, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_152511) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_104719) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -78,23 +78,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_152511) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "ordes", force: :cascade do |t|
-    t.integer "status"
-    t.string "token"
-    t.string "charge_id"
-    t.string "error_message"
-    t.string "customer_id"
-    t.integer "payment_gateway"
+    t.string "state"
+    t.integer "amount"
+    t.string "checkout_session_id"
     t.bigint "user_id", null: false
     t.bigint "formation_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["formation_id"], name: "index_ordes_on_formation_id"
-    t.index ["user_id"], name: "index_ordes_on_user_id"
+    t.index ["formation_id"], name: "index_orders_on_formation_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "temoignages", force: :cascade do |t|
@@ -116,6 +108,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_152511) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "nom"
@@ -127,12 +123,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_152511) do
     t.boolean "newsletter"
     t.string "addresse"
     t.string "email_confirmation"
-    t.boolean "email_confirmed", default: false
-    t.string "confirm_token"
-    t.string "confirmation_token"
-    t.string "unconfirmed_email"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -142,6 +132,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_13_152511) do
   add_foreign_key "formations", "themes"
   add_foreign_key "inscriptions", "formations"
   add_foreign_key "inscriptions", "users"
-  add_foreign_key "ordes", "formations"
-  add_foreign_key "ordes", "users"
+  add_foreign_key "orders", "formations"
+  add_foreign_key "orders", "users"
 end
