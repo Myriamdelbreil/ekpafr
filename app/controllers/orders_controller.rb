1bab52fa38cda9cf3aeef20f7c10d25346bacc48
Myriam Delbreil <myriam.delbreil@live.fr>
2022-07-20 19:21:27 +0200

    back to avant

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,4 @@
 class OrdersController < ApplicationController
-  require 'paypal'
   before_action :paypal_init, :except => [:show, :create]
   skip_before_action :verify_authenticity_token
 
@@ -10,24 +9,22 @@ class OrdersController < ApplicationController
   def create
     formation = Formation.find(params[:formation_id])
     order = Order.create!(formation: formation, amount: formation.prix, state: 'pending', user: current_user)
-    Stripe.api_key = ENV["STRIPE_SECRET_KEY"]
     session = Stripe::Checkout::Session.create(
       payment_method_types: ['card'],
       line_items: [{
         name: formation.titre,
-        amount: formation.prix,
+        amount: formation.prix * 100,
         currency: 'eur',
         quantity: 1
       }],
-      success_url: 'http://ekpafrance.herokuapp.com/inscriptions',
-      cancel_url: 'http://ekpafrance.herokuapp.com'
+      success_url: 'http://www.ekpafrance.com/inscriptions',
+      cancel_url: 'http://www.ekpafrance.com'
     )
     order.update(checkout_session_id: session.id)
     redirect_to new_formation_order_payment_path(formation, order), status: :see_other
   end
 
   def create_order
-    formation = Formation.find(params[:formation_id])
     price = formation.prix
     request = PayPalCheckoutSdk::Orders::OrdersCreateRequest::new
     request.request_body({
